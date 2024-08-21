77b125c2b09c85d972893d647ad884a5639cd4ab
Francisco Bravo <bravofran3@gmail.com>
2024-08-21 16:33:20 +0200

    changed type of options

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,7 +2,7 @@
 
 import * as fastify from 'fastify';
 
-export async function routes (fastify: fastify.FastifyInstance, options: fastify.FastifyRegisterOptions<Options>) {
+export async function routes (fastify: fastify.FastifyInstance, options: fastify.FastifyPluginOptions) {
   fastify.get('/', async (request: fastify.FastifyRequest, reply: fastify.FastifyReply) => {
     return { hello: 'world' }
   })
