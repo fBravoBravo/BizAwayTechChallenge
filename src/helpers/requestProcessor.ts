35fece8fc60cb2598d6f389afeb19bd7d71c976e
Francisco Bravo <bravofran3@gmail.com>
2024-08-21 22:25:03 +0200

    added guard for empty trips return from the API

--- src/helpers/requestProcessor.ts
+++ src/helpers/requestProcessor.ts
@@ -6,16 +6,21 @@ export async function processRequest (origin: string, destination: string, sort_
     let tripData;
     const returnObject: {
         error: boolean,
-        tripData?: Trip[];
+        tripData?: Trip[] | string;
     } = {
         error: false
     }
 
     try {
         tripData = await fetchTrip(origin, destination);
+        //TODO be careful with empty list of trips in the sort.
     } catch (error) {
         returnObject.error = true;
         return returnObject;
+    }
+
+    if (tripData.length === 0) {
+        returnObject.tripData = "No trips found for the given origin and destination.";
     }
 
     if (sort_by === "fastest") {
