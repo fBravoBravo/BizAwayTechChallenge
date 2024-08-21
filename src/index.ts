0607b27bb59cb3ba0231de1f28cd3a55ac037987
Francisco Bravo <bravofran3@gmail.com>
2024-08-21 16:04:15 +0200

    fastify working

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
-import { db } from "./database/datbaseConnector.js";
+// Require the framework and instantiate it
 
-console.log(db);
+// ESM
+import Fastify from 'fastify'
+
+const fastify = Fastify({
+  logger: true
+})
+
+// Declare a route
+fastify.get('/', function (_, reply) {
+  reply.send({ hello: 'world' })
+})
+
+// Run the server!
+fastify.listen({ port: 3000 }, function (err) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  // Server is now listening on ${address}
+})
