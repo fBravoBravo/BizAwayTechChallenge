020561d2a4ec6e51101b3580034fb5b44543949c
Francisco Bravo <bravofran3@gmail.com>
2024-08-23 12:02:53 +0200

    created rows in databasae

--- src/database/datbaseConnector.ts
+++ src/database/datbaseConnector.ts
@@ -1,10 +1,12 @@
 import sqlite3 from 'sqlite3';
 
-// open the database
-export const db = new sqlite3.Database('./database.db', sqlite3.OPEN_READWRITE, (err) => {
+export async function initializeDbConnection (){
+  const db = new sqlite3.Database('./database.db', sqlite3.OPEN_READWRITE, (err) => {
   if (err) {
     console.error(err.message);
     return;
   }
   console.log('Connected to the database.');
+  return db;
 });
+}
