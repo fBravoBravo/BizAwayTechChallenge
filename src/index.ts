4ed3f7356a2f277106165ddb1968ad91968a4cd7
Francisco Bravo <bravofran3@gmail.com>
2024-08-27 12:37:26 +0200

    added swagger to the server

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,34 @@
 import Fastify from 'fastify'
 import { routes } from './api/routes/routes.js';
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 
 const server = Fastify({
   logger: true
 })
+
+const swaggerOptions = {
+    swagger: {
+        info: {
+            title: "My Title",
+            description: "My Description.",
+            version: "1.0.0",
+        },
+        host: "localhost",
+        schemes: ["http", "https"],
+        consumes: ["application/json"],
+        produces: ["application/json"],
+        tags: [{ name: "Default", description: "Default" }],
+    },
+};
+
+const swaggerUiOptions = {
+    routePrefix: "/docs",
+    exposeRoute: true,
+};
+
+server.register(fastifySwagger, swaggerOptions);
+server.register(fastifySwaggerUi, swaggerUiOptions);
 
 server.register(routes, { prefix: '/api' });
 
